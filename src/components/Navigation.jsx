fa6d66296801977dd76f3a50633fe660a5c75e00
Grace Adelaka <adelakagrace@gmail.com>
2022-06-14 16:47:52 +0100

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Logo from "../assets/logo.svg";
+import Cart from "../assets/icon-cart.svg";
+import Avatar from "../assets/image-avatar.png";
+
+const Navigation = () => {
+  return (
+    <div className="max-w-[1440px] flex justify-between mx-auto bg-[#f8f9fa] h-20 px-6 items-center">
+      <div className="flex">
+        <div>
+          <img src={Logo} className="pt-2" alt="logo" />
+        </div>
+        <ul className="flex text-lg ml-9 text-[#2c2d2ea6]">
+          <li className="px-[25px] py-[6px] hover:border-b-2 hover:border-b-[#f1730c]">
+            Collections
+          </li>
+          <li className="px-[25px] py-[6px] hover:border-b-2 hover:border-b-[#f1730c]">
+            Women
+          </li>
+          <li className="px-[25px] py-[6px] hover:border-b-2 hover:border-b-[#f1730c]">
+            Men
+          </li>
+          <li className="px-[25px] py-[6px] hover:border-b-2 hover:border-b-[#f1730c]">
+            About
+          </li>
+          <li className="px-[25px] py-[6px] hover:border-b-2 hover:border-b-[#f1730c]">
+            Contact
+          </li>
+        </ul>
+      </div>
+      <div className="flex">
+        <img
+          src={Cart}
+          className="relative w-[50%] h-[50%] pt-2 mr-10"
+          alt="shopping cart"
+        />
+
+        <img
+          src={Avatar}
+          className="w-[40px] hover:border-2 hover:rounded-[50%] hover:border-[#f1730c]"
+          alt="user avatar"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
