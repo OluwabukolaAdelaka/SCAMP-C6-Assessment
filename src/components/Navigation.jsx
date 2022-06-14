ba8c46253611fea2f0e09f0dd347425f95238a79
Grace Adelaka <adelakagrace@gmail.com>
2022-06-14 17:26:50 +0100

    updated navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import Logo from "../assets/logo.svg";
 import Cart from "../assets/icon-cart.svg";
 import Avatar from "../assets/image-avatar.png";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navigation = () => {
   return (
     <div className="max-w-[1440px] flex justify-between mx-auto bg-[#f8f9fa] h-20 px-6 items-center">
       <div className="flex">
+        <GiHamburgerMenu size={40} className="pr-4" />
         <div>
           <img src={Logo} className="pt-2" alt="logo" />
         </div>
-        <ul className="flex text-lg ml-9 text-[#2c2d2ea6]">
+        {/* desktop */}
+        <ul className="hidden md:flex text-lg ml-9 text-[#2c2d2ea6]">
           <li className="px-[25px] py-[6px] hover:border-b-2 hover:border-b-[#f1730c]">
             Collections
           </li>
@@ -28,10 +31,11 @@ const Navigation = () => {
           </li>
         </ul>
       </div>
+
       <div className="flex">
         <img
           src={Cart}
-          className="relative w-[50%] h-[50%] pt-2 mr-10"
+          className="relative w-[50%] h-[50%] pt-3 mr-6 md:mr-8"
           alt="shopping cart"
         />
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {colors: {
+      primary: {
+        100: '#f1730c',
+        200: '#f1730cca'
+      },
+      secondary: {
+        100: '#1d2026',
+        200: '#2c2d2ea6'
+      },
+      fontFamily: {
+        body: ['Arial, Helvetica, sans-serif']
+      }
+    }},
   },
   plugins: [],
 }
